81e2b7e22da91fb6c061ea47104b5ecff34a7bbe
Daniel Reszka <dannn.r@gmail.com>
2021-10-03 18:12:28 +0200

    components: add Button

---
+++ src/components/forms/Button.svelte
@@ -0,0 +1,24 @@
+<script>
+	export let type = 'button'
+	const className = $$props.class ?? 'text-white bg-blue-500'
+</script>
+
+<button {type} class={className}>
+	<slot />
+</button>
+
+<style lang="postcss">
+	button {
+		@apply w-full flex justify-center items-center;
+		@apply px-3 py-2;
+		@apply rounded-md text-sm font-medium;
+		@apply opacity-80 hover:opacity-100 focus:opacity-100 focus:outline-none;
+		@apply transition-opacity duration-200;
+	}
+	button > :global(*) {
+		@apply mx-[0.2em];
+	}
+	button > :global(svg) {
+		@apply flex-shrink-0 w-4 h-4;
+	}
+</style>
