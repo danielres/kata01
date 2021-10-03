b0b32f5c9f7166a70c59fe751da9275d32a1f444
Daniel Reszka <dannn.r@gmail.com>
2021-10-03 18:15:39 +0200

    components: add Backdrop

---
+++ src/components/Backdrop.svelte
@@ -0,0 +1,9 @@
+<div><slot /></div>
+
+<style lang="postcss">
+	div {
+		@apply flex min-h-screen items-center justify-center;
+		@apply px-4 sm:px-6 lg:px-8 py-12;
+		@apply bg-gray-200;
+	}
+</style>
