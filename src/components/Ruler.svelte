1e16f98c4377cb1a154ce8a4545e544d5d920be3
Daniel Reszka <dannn.r@gmail.com>
2021-10-03 18:12:10 +0200

    components: add Ruler

---
+++ src/components/Ruler.svelte
@@ -0,0 +1,14 @@
+<div class="flex items-center justify-between">
+	<span class="line" />
+	<span class="text"> <slot>Text</slot></span>
+	<span class="line" />
+</div>
+
+<style lang="postcss">
+	.line {
+		@apply w-1/4 border-b border-gray-300 lg:w-1/4;
+	}
+	.text {
+		@apply text-xs text-center text-gray-600 uppercase;
+	}
+</style>
