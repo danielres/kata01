602b386550e3eb6015da91ef9d6ed72aa826b1eb
Daniel Reszka <dannn.r@gmail.com>
2021-10-05 01:01:09 +0200

    actions: add use:todo

---
+++ src/actions/todo.js
@@ -0,0 +1,9 @@
+import { tooltip } from '../components/Tooltip.svelte'
+
+export const todo = (node, opts) => {
+	node.onclick = (e) => e.preventDefault()
+	node.classList.add('hover:ring-1')
+	node.classList.add('ring-offset-4')
+	node.classList.add('ring-gray-600')
+	return tooltip(node, { ...opts, text: 'Todo' })
+}
