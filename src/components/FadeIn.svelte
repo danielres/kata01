43260fe4bb66a09f16fa9cb46257cb36f5fb31c4
Daniel Reszka <dannn.r@gmail.com>
2021-10-04 05:20:13 +0200

    components: add FadeIn

---
+++ src/components/FadeIn.svelte
@@ -0,0 +1,12 @@
+<script>
+	import { fade } from 'svelte/transition'
+
+	let visible = false
+	setTimeout(() => (visible = true), 250)
+</script>
+
+{#if visible}
+	<div in:fade={{ duration: 1000 }}>
+		<slot />
+	</div>
+{/if}
